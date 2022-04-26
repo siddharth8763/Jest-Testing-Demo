250794b0d365c2b622d0337bc258028d89ee825b
siddharth8763 <46642995+siddharth8763@users.noreply.github.com>
2022-04-26 13:15:32 +0530

    Files for init commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div data-test="component-app" className="App">
+      <h1 data-test="counter-display">
+        The counter is currently&nbsp; 
+        <span data-test="count">{count}</span>
+      </h1>
+      <button
+        data-test="increment-button"
+        onClick={() => setCount(count + 1)}
+        onDoubleClick={()=>setCount(0)}
+      >
+        Increment counter
+      </button>
+    </div>
+  );
+}
+
+export default App;
